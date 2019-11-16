63471ae8c1cbc0078f7cedaad9e5d6428a7bafe3
EmiSchaufeld <ebs338@nyu.edu>
2019-11-16 18:57:34 -0500

    camera swap lol

---
+++ TimeWithEmi/Assets/Scripts/CameraSwap.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraSwap : MonoBehaviour
+{
+    public Camera camOne;
+    public Camera camTwo;
+    // Start is called before the first frame update
+    void Start()
+    {
+        camOne.enabled = true;
+        camTwo.enabled = false;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position.y < -1)
+        {
+            camTwo.enabled = true;
+            camOne.enabled = false;
+        }
+        else
+        {
+            camOne.enabled = true;
+            camTwo.enabled = false;
+        }
+
+    }
+}
+   
