7941328453b50320855a2cae7ba1588caaab1953
EmiSchaufeld <ebs338@nyu.edu>
2019-11-17 21:07:35 -0500

    didn't push everything

---
+++ TimeWithEmi/Assets/Scripts/ColColorChange.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColColorChange : MonoBehaviour
+{
+    public string ringName; //name the ring in inspect
+                            // Start is called before the first frame update
+
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        GameObject ring = GameObject.Find(ringName);  //change the color on the ring
+        ColorChange colorChanger = ring.GetComponent<ColorChange>();
+        colorChanger.ChangeColor();
+        AudioSource score = GetComponent<AudioSource>();
+        score.Play();
+    }
+}
---
+++ TimeWithEmi/Assets/Scripts/ColorChange.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColorChange : MonoBehaviour
+{
+    public Material[] materials;
+    MeshRenderer meshRenderer;
+    int materialNumber = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        meshRenderer = GetComponent<MeshRenderer>(); 
+
+        meshRenderer.material = materials[0]; //start at 0
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+  
+
+    public void ChangeColor()
+    {
+        materialNumber++; //go through the mat array
+        meshRenderer.material = materials[materialNumber % materials.Length];
+    }
+}
